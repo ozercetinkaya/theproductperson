e2d93bb450f3719b42d3b8f4530f2c9947f3a9e5
end-y <enderyazici@yandex.com>
2025-09-21 19:06:03 +0300

    fix

--- src/app/blog/detail/page.js
+++ src/app/blog/detail/page.js
@@ -16,8 +16,8 @@ import React from "react";
 import Image from "next/image";
 import { getMediaUrl } from "@/utils";
 
-export default async function BlogDetail() {
-  const slug = "1";
+export default async function BlogDetail({ params }) {
+  const slug = params?.name;
   const STRAPI_URL =
     process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337";
   const CT = "articles";
