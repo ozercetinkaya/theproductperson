3c77bb060214ba1eababc1fc490d5385eaa36f51
end-y <enderyazici@yandex.com>
2025-09-21 17:54:15 +0300

    fix

--- src/app/blog/detail/page.js
+++ src/app/blog/detail/page.js
@@ -4,5 +4,12 @@ import Image from "next/image";
 
 export default async function BlogDetail({ params }) {
   const slug = params?.slug;
+
+  const STRAPI_URL =
+    process.env.NEXT_PUBLIC_STRAPI_URL_LIVE ||
+    "https://theproductpersonbackend-production.up.railway.app" ||
+    "http://localhost:1337";
+  const CT = "articles";
+
   return <div>BlogDetail</div>;
 }
